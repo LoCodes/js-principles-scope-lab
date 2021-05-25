c3239816b7e5ffac5d0263960ca25fd940299cd5
Loren Cipriano <loren25cipriano@gmail.com>
2021-05-25 14:59:58 -0700

    commit tree e24917638b9704bd5f52ad036db64e1e9be2e244

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'initial value'
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    // console.log(customerName.toUpperCase());
+    // console.log(sentence.toUpperCase());
+
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'
+}
